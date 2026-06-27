edd489261fb6ac34f124dd54f8df551d6262a48f
Yunus Emre Gürlek <105203804+Dpehect@users.noreply.github.com>
2026-06-27 14:44:21 +0300

    Initial Commit

---
+++ Muhasebe/MuhasebeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuhasebeApp.swift
+//  Muhasebe
+//
+//  Created by Yunus Emre Gürlek on 27.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct MuhasebeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MuhasebeTests/MuhasebeTests.swift
@@ -0,0 +1,18 @@
+//
+//  MuhasebeTests.swift
+//  MuhasebeTests
+//
+//  Created by Yunus Emre Gürlek on 27.06.2026.
+//
+
+import Testing
+
+struct MuhasebeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
